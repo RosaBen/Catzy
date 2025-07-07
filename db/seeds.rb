b4fa65dd94706bf57d027a3013d93aa42e44a22d
RosaBen <benchabane.rosa@outlook.com>
2025-07-07 19:15:25 +0200

    fixed image on show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,34 +10,35 @@ puts "✅ Données nettoyées."
 
 puts "📦 Création de 20 articles..."
 
-kittens_url =[
-"https://www.pexels.com/fr-fr/photo/photo-en-gros-plan-d-un-chat-endormi-mignon-416160/",
-"https://www.pexels.com/fr-fr/photo/chaton-en-fourrure-grise-127028/",
-"https://www.pexels.com/fr-fr/photo/gros-plan-de-la-photographie-de-chaton-brun-et-blanc-1870376/",
- "https://www.pexels.com/fr-fr/photo/photo-en-gros-plan-de-personne-tenant-un-chaton-blanc-1444321/",
- "https://www.pexels.com/fr-fr/photo/chat-tigre-orange-allonge-sur-le-sol-290164/",
- "https://www.pexels.com/fr-fr/photo/chaton-gris-et-blanc-sur-lit-blanc-2061057/",
- "https://www.pexels.com/fr-fr/photo/chaton-noir-et-blanc-sur-textile-marron-96938/",
-"https://www.pexels.com/fr-fr/photo/low-angle-view-of-cat-sur-l-arbre-257532/",
-"https://www.pexels.com/fr-fr/photo/chaton-bleu-russe-sur-panier-tisse-marron-127027/",
-"https://www.pexels.com/fr-fr/photo/photographie-en-gros-plan-de-chat-tigre-endormi-1056251/",
-"https://www.pexels.com/fr-fr/photo/photographie-en-gros-plan-d-un-chat-blanc-en-plus-des-lumieres-de-noel-735423/",
-"https://www.pexels.com/fr-fr/photo/chaton-blanc-et-gris-sentant-la-fleur-de-marguerite-blanche-1472999/",
-"https://www.pexels.com/fr-fr/photo/photo-de-chaton-tigre-gris-et-blanc-assis-sur-un-canape-2194261/",
-"https://www.pexels.com/fr-fr/photo/photo-de-chaton-tigre-gris-couche-2558605/",
-"https://www.pexels.com/fr-fr/photo/chatons-de-couleurs-assorties-45170/",
-"https://www.pexels.com/fr-fr/photo/photo-en-gros-plan-d-une-personne-qui-nourrit-un-chaton-1981111/",
-"https://www.pexels.com/fr-fr/photo/chaton-blanc-et-noir-allonge-sur-des-carreaux-479009/",
-"https://www.pexels.com/fr-fr/photo/chat-tigre-gris-allonge-sur-des-guirlandes-blanches-923360/",
-"https://www.pexels.com/fr-fr/photo/gros-plan-de-la-photographie-de-chaton-blanc-171227/",
-"https://www.pexels.com/fr-fr/photo/chat-calico-sur-focus-photo-1404819/"
+kittens=[
+"https://images.pexels.com/photos/596590/pexels-photo-596590.jpeg",
+"https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg",
+"https://images.pexels.com/photos/1870376/pexels-photo-1870376.jpeg",
+ "https://images.pexels.com/photos/1444321/pexels-photo-1444321.jpeg",
+ "https://images.pexels.com/photos/290164/pexels-photo-290164.jpeg",
+ "https://images.pexels.com/photos/127028/pexels-photo-127028.jpeg",
+ "https://images.pexels.com/photos/2061057/pexels-photo-2061057.jpeg",
+ "https://images.pexels.com/photos/96938/pexels-photo-96938.jpeg",
+"https://images.pexels.com/photos/257532/pexels-photo-257532.jpeg",
+"https://images.pexels.com/photos/127027/pexels-photo-127027.jpeg",
+"https://images.pexels.com/photos/1056251/pexels-photo-1056251.jpeg",
+"https://images.pexels.com/photos/735423/pexels-photo-735423.jpeg",
+"https://images.pexels.com/photos/1472999/pexels-photo-1472999.jpeg",
+"https://images.pexels.com/photos/2194261/pexels-photo-2194261.jpeg",
+"https://images.pexels.com/photos/2558605/pexels-photo-2558605.jpeg",
+"https://images.pexels.com/photos/669015/pexels-photo-669015.jpeg",
+"https://images.pexels.com/photos/1981111/pexels-photo-1981111.jpeg",
+"https://images.pexels.com/photos/479009/pexels-photo-479009.jpeg",
+"https://images.pexels.com/photos/171227/pexels-photo-171227.jpeg",
+"https://images.pexels.com/photos/923360/pexels-photo-923360.jpeg"
 ]
+
 20.times do
 Item.create!(
     title: "chaton - "+Faker::Emotion.adjective,
     description: Faker::Lorem.paragraph(sentence_count: 2),
     price: Faker::Commerce.price(range: 0..100.0, as_string: true),
-    image_url: kittens_url.sample
+    image_url: kittens.sample
   )
 end
 puts "✅ Kittens ajoutés."
