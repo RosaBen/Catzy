47063626edbc2b40da216af6b321d7bcf9ebdd4f
RosaBen <benchabane.rosa@outlook.com>
2025-07-07 17:08:56 +0200

    added devise and seed

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :encrypted_password, presence: true, length: { minimum: 6 }
+
+  def full_name
+    "#{first_name} #{last_name}".strip
+  end
+end
