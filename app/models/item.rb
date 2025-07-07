f0b90990493be983dbd6ec703eab1f5d7899e436
brainycodings <brainycodings@gmail.com>
2025-07-07 17:59:51 +0200

    correction typo validation dans le model Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,5 @@ class Item < ApplicationRecord
   validates :description, length: { maximum: 500 }
   validates :title, length: { maximum: 20 }
   validates :price, :image_url, :title, presence: true
-  validates :price, numerically: { greater_than: 0 }
+  validates :price, numericality: { greater_than: 0 }
 end
