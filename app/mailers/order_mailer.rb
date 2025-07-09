8084a28784e32d07cb6d1e1273bab96cafc2050b
melody-ste <melody_stephan@icloud.com>
2025-07-09 17:38:02 +0200

    adding def in orderMailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,2 +1,15 @@
 class OrderMailer < ApplicationMailer
+  default from: 'catzy957@gmail.com'
+
+  def order_confirmation(order)
+    
+    @order = order 
+    @user = @order.user
+
+    #on définit une variable @url qu'on utilisera dans la view d’e-mail
+    # @url  = 'http://monsite.fr/login' 
+
+    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+    mail(to: @user.email, subject: 'Confirmation de votre commande ##{@order.id}') 
+  end
 end
