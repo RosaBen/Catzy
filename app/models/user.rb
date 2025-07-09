b1d6c33f1cc15c11711e3b6dbd4759e27a8231cb
melody-ste <melody_stephan@icloud.com>
2025-07-09 10:34:36 +0200

    minor links changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :encrypted_password, presence: true, length: { minimum: 6 }
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "items#index"
-  # get "items/index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  # get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
-
+  
   resources :items
-  resources :profils
+  # resource :profil
   resources :avatars, only: [ :create, :update, :destroy ]
 
   post 'checkout/create', to: 'checkout#create'
