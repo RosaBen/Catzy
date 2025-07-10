b512f0d4ceaa8c2cfa05f9cc928fadb4d847d895
brainycodings <brainycodings@gmail.com>
2025-07-10 13:06:41 +0200

    paiement

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -18,7 +18,36 @@ class CheckoutController < ApplicationController
       success_url: root_url + "?success=true",
       cancel_url: root_url + "?canceled=true",
     )
+    end
+     
+  def create_cart_checkout
+    cart = current_user.cart
+
+    if cart.blank? || cart.items.empty?
+      redirect_to cart_path, alert: "Votre panier est vide."
+      return
+    end
+
+    session = Stripe::Checkout::Session.create(
+      payment_method_types: ['card'],
+      line_items: cart.items.map do |item|
+        {
+          price_data: {
+            currency: 'eur',
+            product_data: {
+              name: item.title
+            },
+            unit_amount: (item.price.to_f * 100).round
+          },
+          quantity: 1
+        }
+      end,
+      mode: 'payment',
+      success_url: order_success_url,
+      cancel_url: cart_url
+    )
 
     redirect_to session.url, allow_other_host: true
   end
+  # comment
 end
