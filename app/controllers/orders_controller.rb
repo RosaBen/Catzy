795483e7d4c015d23ee83a86ad6a496161f0e897
brainycodings <brainycodings@gmail.com>
2025-07-10 14:26:39 +0200

    récapitulatif commande

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,19 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def success
-    flash[:notice] = "Merci pour votre commande !"
+    cart = current_user.cart
+
+    if cart && cart.items.any?
+      # Créer la commande
+      @order = current_user.orders.create
+
+      cart.items.each do |item|
+        OrderItem.create(order: @order, item: item)
+      end
+
+      # Vider le panier
+      cart.items.clear
+
+    end
   end
 end
